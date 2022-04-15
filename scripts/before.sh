a7a8cc44aa6127ed4162cc6b46aa930c2c54a085
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2022-04-14 22:58:58 -0500

    Update before.sh

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,13 +1,13 @@
 ### before ####
 #!/bin/bash
-sudo yum update -y
+#sudo yum update -y
 
 #### install nginx ###
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl enable nginx
-sudo systemctl restart nginx
+#sudo amazon-linux-extras install nginx1 -y
+#sudo systemctl enable nginx
+#sudo systemctl restart nginx
 
-sudo rm -rf /tmp/dockernodejs
+#sudo rm -rf /tmp/dockernodejs
 
 ### security practices of aws ci/cd
 
